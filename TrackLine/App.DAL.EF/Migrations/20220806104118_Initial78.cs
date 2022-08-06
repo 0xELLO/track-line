371311889e00d6216a8947f7451eb91a594dc179
repl1cator101 <73614603+repl1cator101@users.noreply.github.com>
2022-08-06 14:11:34 +0300

    Fixed naming bugs

---
+++ TrackLine/App.DAL.EF/Migrations/20220806104118_Initial78.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.DAL.EF.Migrations
+{
+    public partial class Initial78 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FooBar",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    CratedBy = table.Column<string>(type: "TEXT", maxLength: 32, nullable: true),
+                    CratedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "TEXT", maxLength: 32, nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FooBar", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FooBar");
+        }
+    }
+}
